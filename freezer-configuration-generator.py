02844b419fca25e8c98e97f0646ddb9236b4797b
JP Cartailler <c5creative@users.noreply.github.com>
2018-09-13 14:36:03 -0500

    CORELIMS-63

--- freezer-configuration-generator.py
+++ freezer-configuration-generator.py
@@ -17,7 +17,7 @@ import os
 # ----------------------------------------------------------------------------------------------------------------------
 # CONFIGURATION START
 #
-yaml_file = 'configurations/reconfigure_freezer2-shelf1.yaml'
+yaml_file = 'configurations/freezer_01_add_rack_toshelf2.yaml'
 
 try:
     containers_config = yaml.load(file(yaml_file, 'r'))
